7d5ca47046f4e61dc30ab62d21a9a6cb887b4fe0
Aleksandar <accam003@gmail.com>
2025-10-30 14:16:57 +0100

    Ažurirao sam `application.properties` za `api-gateway`, `bookings-service` i `user-service` da poboljšam otkrivanje servisa i podršku za Actuator. Dodao sam konfiguraciju za Eureku i jasnije komentare. Izmenio sam `UserClient` da pojednostavim Feign klijent putanju. U `bookings-service` sam dodao Actuator zavisnosti radi boljeg praćenja.

--- bookings-service/src/main/java/com/example/bookingsservice/client/UserClient.java
+++ bookings-service/src/main/java/com/example/bookingsservice/client/UserClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", path = "/api/users")
+@FeignClient(name = "user-service")
 public interface UserClient {
 
     @GetMapping("/{id}")
